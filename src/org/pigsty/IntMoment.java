735a0a7bb3834273d87f6c3b7884b756900181b2
Peng Xie <xiepeng.peng@gmail.com>
2014-10-13 22:01:01 +0000

    rename

--- src/org/pigsty/IntMoment.java
+++ src/org/pigsty/IntMoment.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.sabre.research.pig.pigsty;
+package org.pigsty;
 
 import java.io.IOException;
 import java.util.HashMap;
